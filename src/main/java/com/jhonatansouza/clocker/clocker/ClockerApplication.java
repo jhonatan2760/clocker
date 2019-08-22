06861518cf6cd4a457917a65c868cd334d54fa9b
Jhonatan Souza <jhonatan.souza@b2wdigital.com>
2019-08-22 16:28:12 -0300

    Initial commit

---
+++ src/main/java/com/jhonatansouza/clocker/clocker/ClockerApplication.java
@@ -0,0 +1,13 @@
+package com.jhonatansouza.clocker.clocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockerApplication.class, args);
+	}
+
+}
