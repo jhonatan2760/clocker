981a4eb523898faa484ec6daf88e7887844f23db
jhonatansilvadesousa <jhonatan2760@gmail.com>
2019-08-27 10:39:06 -0300

    Alteracoes

---
+++ src/main/java/com/jhonatansouza/clocker/clocker/model/DeviceEntity.java
@@ -0,0 +1,44 @@
+package com.jhonatansouza.clocker.clocker.model;
+
+import java.time.LocalDateTime;
+
+public class DeviceEntity {
+
+    private Long id;
+    private String name;
+    private boolean status;
+    private LocalDateTime createdAt;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
